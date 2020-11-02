b7284e3b6a507f4545a7664fd07a305aa861023d
karirogg <karirogg@gmail.com>
2020-11-02 08:55:34 +0000

    publish 2020-10-29

--- R_mynd.R
+++ R_mynd.R
@@ -30,8 +30,8 @@ height=6.7
 diff=0.5
 extra_diff=2
 pandemic_start_date <- '2020-02-28'
-#examine_start_date = '2020-02-28'
-examine_start_date='2020-07-17'
+examine_start_date = '2020-02-28'
+#examine_start_date='2020-07-17'
 end_date = Sys.Date()
 
 intervention_dat <- tibble(date=c(ymd(c("2020-03-16","2020-03-24","2020-05-04","2020-06-15","2020-07-31","2020-08-19",'2020-09-07', '2020-09-18', '2020-10-07'), Sys.Date())),
@@ -45,8 +45,7 @@ intervention_dat <- tibble(date=c(ymd(c("2020-03-16","2020-03-24","2020-05-04","
 intervention_dat$lab_pos_x[1] <- ymd('2020-03-01')
 if(examine_start_date == '2020-02-28') {
     intervention_dat$lab_pos_x[8] <- ymd('2020-09-18')+1
-    intervention_dat$lab_pos_x[7] <- ymd('2020-08-23')
-    intervention_dat$lab_pos_x[9] <- ymd('2020-09-26')
+    intervention_dat$lab_pos_x[7] <- ymd('2020-08-22')
 } else {
     intervention_dat$lab_pos_x[7] <- ymd('2020-08-31')
     intervention_dat$lab_pos_x[8] <- ymd('2020-09-18')+1
@@ -141,7 +140,7 @@ plot_grid(rp, lp, ncol = 1, align='v') +
     ggsave(here("Results", "Figures",'R_t', paste0("R_and_local_cases_",Sys.Date(),".png")), device = "png", 
            height = 4.5 * 2, width = 19)
 
-   R_q_dat <- spread_draws(m,log_R_q) %>%
+R_q_dat <- spread_draws(m,log_R_q) %>%
     mutate(iter = row_number()) %>%
     mutate(R_q=exp(log_R_q)) %>%
     select(iter,R_q) %>%
@@ -168,12 +167,38 @@ exp_fit_march <- R_summary %>% filter(date > ymd('2020-03-24'), date <= ymd('202
 march_lowering <- coef(exp_fit_march)['day'] %>% as.numeric %>% exp
 
 scenarios = c('constant', 'lowering')
+examine_start_date = '2020-07-17'
 
-for(scene in scenarios) {
+intervention_dat <- tibble(date=c(ymd(c("2020-03-16","2020-03-24","2020-05-04","2020-06-15","2020-07-31","2020-08-19",'2020-09-07', '2020-09-18', '2020-10-07'), Sys.Date())),
+                           lab=c('Samkomur,takmarkaðar,við 100 manns','Samkomur,takmarkaðar,við 20 manns','Fjöldatakmarkanir,rýmkaðar'
+                                 ,'Landamæraskimun,hefst','Samkomur,takmarkaðar,við 100 manns','Sóttkví milli,tveggja skimanna,á landamærunum'
+                                 ,'Eins metra regla,í stað tveggja,metra reglu', 'Skemmtistöðum,og krám á,höfuðborgar-,svæðinu lokað,tímabundið', 'Hertar,samkomu-,takmarkanir', ''),
+                           lab_pos_x=date,
+                           lab_pos_y=height)
+
+# Heights of labels adjusted if R value is too high
+intervention_dat$lab_pos_x[1] <- ymd('2020-03-01')
+if(examine_start_date == '2020-02-28') {
+    intervention_dat$lab_pos_x[8] <- ymd('2020-09-18')+1
+    intervention_dat$lab_pos_x[7] <- ymd('2020-08-22')
+    intervention_dat$lab_pos_x[9] <- ymd('2020-09-25')
+} else {
+    intervention_dat$lab_pos_x[7] <- ymd('2020-08-31')
+    intervention_dat$lab_pos_x[8] <- ymd('2020-09-18')+1
+}
+intervention_dat$lab_pos_y[7] <- height+extra_diff
+intervention_dat$lab_pos_y[8] <- height+extra_diff
+intervention_dat$lab_pos_y[9] <- height-1
+
+intervention_lab_dat <- separate_rows(intervention_dat,lab,sep=',') %>% 
+    group_by(date) %>% 
+    mutate(lab_pos_y=seq(lab_pos_y[1],lab_pos_y[1]-diff*(n()-1),by=-diff))
+
+for(scene in 1:2) {
     future_R <- function(R_t,t) {
         R_t
     }
-    if(scene == 'lowering') {
+    if(scene == 2) {
         future_R <- function(R_t,t) {
             R_t*march_lowering^t
         }   
@@ -185,12 +210,17 @@ for(scene in scenarios) {
     
     posterior_dat <- scenario(m=m,d=d, R_fun = future_R, future_prop_quarantine=rep(0.5, pred_days-1), prop_imported=0,pred_days=pred_days, fit_date=fit_date)
     
+    posterior_name <- paste0("Iceland_posterior_",Sys.Date(), '.csv')
+    if(scene != 1) {
+        posterior_name <- paste0("Iceland_posterior_",Sys.Date(),scene ,'.csv')
+    }
+    
     posterior_csv <- posterior_dat %>% filter(name=='y_hat') %>%
         mutate(date=ymd('2020-02-28')+day) %>%
         select(iter,date, value) %>%
         rename(new_cases=value) %>%
         filter(date >= fit_date) %>%
-        write_csv(here('Results', 'Data', paste0("Iceland_posterior_",scene,"_R_", Sys.Date(), '.csv')))
+        write_csv(here('Results', 'Data', posterior_name))
     
     plot_dat <- posterior_dat %>% summarize_posterior_dat
     
@@ -213,26 +243,26 @@ for(scene in scenarios) {
     pred_rp
     
     #Retrofitted local cases - prediction and history
-    pred_lp <-  plot_dat %>% filter(name=='y_hat', prob < 95) %>%
+    pred_lp <-  plot_dat %>% filter(name=='y_hat', prob<95) %>%
         ggplot(aes(date, ymin = lower, ymax = upper)) +
         geom_ribbon(aes(fill = factor(-prob)), alpha = 0.7) +
         geom_point(data = d, aes(x = date, y = local), inherit.aes = F) +
         geom_vline(xintercept = as.numeric(intervention_dat$date), lty = 2) +     
         scale_fill_brewer() +
-        scale_x_date(date_breaks = "month",
-                     date_labels = "%B %d",
+        scale_x_date(limits = c(ymd("2020-07-17"), ymd(end_date) + pred_days-1), 
                      expand = expansion(add = 0),
-                     limits = c(ymd("2020-07-17"), ymd(end_date) + pred_days-1)) +
-        scale_y_continuous(breaks = pretty_breaks(8), expand = expansion(mult = 0), limits=c(0,350)) +
+                     breaks = sort(c(fill_up_dates, intervention_dat$date)),
+                     labels = icelandic_dates) +
+        scale_y_continuous(breaks = pretty_breaks(8), 
+                           expand = expansion(mult = 0)) +
+        coord_cartesian(ylim = c(0, 350)) +
         labs(subtitle = "Innlend dagleg smit") +
         theme(axis.title = element_blank(),
-              axis.ticks.x = element_blank(),
-              axis.text.x = element_blank(),
               plot.margin = margin(5, 5, 5, 11),
               legend.title = element_blank()) 
     pred_lp
     plot_grid(pred_rp, pred_lp, ncol = 1, align='v') +
-        ggsave(here("Results", "Figures", "Predictions", paste0('Prediction_',scene,'_R_and_local_cases_',Sys.Date(),'.png')), device = 'png', 
+        ggsave(here("Results", "Figures", "Predictions", paste0('Prediction_',scenarios[scene],'_R_and_local_cases_',Sys.Date(),'.png')), device = 'png', 
                height = 4.5 * 2, width = 19)
 }
 
@@ -268,27 +298,27 @@ plot_dat <- posterior_dat %>% summarize_posterior_dat
 
 point_d = d %>% mutate(point_color=if_else(date > ymd('2020-09-28'), 'orange', 'black'))
 
-write.csv(plot_dat,file='local_cases_prediction_data.csv', sep=',', row.names=F, quote=F)
+#write.csv(plot_dat,file='local_cases_prediction_data.csv', sep=',', row.names=F, quote=F)
 
 #Comparison plot for old prediction
-compare_pred_lp <- plot_dat %>% filter(name=='y_hat') %>%
-    ggplot(aes(date, ymin = lower, ymax = upper)) +
-    geom_ribbon(aes(fill = factor(-prob)), alpha = 0.7) +
-    geom_point(data = point_d, aes(x = date, y = local, color=point_color), inherit.aes = F) +
-    scale_color_manual(values=c('orange'='#ff8c00', 'black'='black')) + 
-    scale_fill_brewer() +
-    scale_x_date(breaks = sort(c(ymd('2020-09-16'), ymd('2020-10-01'), ymd('2020-10-16'))),
-                labels = icelandic_dates,
-                expand = expansion(add = 0),
-                limits = c(ymd('2020-09-14'), ymd('2020-10-20'))) +
-    scale_y_continuous(breaks = pretty_breaks(8), expand = expansion(mult = 0)) +
-    labs(subtitle = "Innlend dagleg smit") +
-    theme(axis.title = element_blank(),
-          plot.margin = margin(5, 5, 5, 11),
-          legend.title = element_blank()) 
-compare_pred_lp
-
-ggsave(here('Results','Figures','Other', paste0('Local_cases_comparison_',Sys.Date(),'.png')),height=4.5,width=16,device='png')
+# compare_pred_lp <- plot_dat %>% filter(name=='y_hat') %>%
+#     ggplot(aes(date, ymin = lower, ymax = upper)) +
+#     geom_ribbon(aes(fill = factor(-prob)), alpha = 0.7) +
+#     geom_point(data = point_d, aes(x = date, y = local, color=point_color), inherit.aes = F) +
+#     scale_color_manual(values=c('orange'='#ff8c00', 'black'='black')) + 
+#     scale_fill_brewer() +
+#     scale_x_date(breaks = sort(c(ymd('2020-09-16'), ymd('2020-10-01'), ymd('2020-10-16'))),
+#                 labels = icelandic_dates,
+#                 expand = expansion(add = 0),
+#                 limits = c(ymd('2020-09-14'), ymd('2020-10-20'))) +
+#     scale_y_continuous(breaks = pretty_breaks(8), expand = expansion(mult = 0)) +
+#     labs(subtitle = "Innlend dagleg smit") +
+#     theme(axis.title = element_blank(),
+#           plot.margin = margin(5, 5, 5, 11),
+#           legend.title = element_blank()) 
+# compare_pred_lp
+# 
+# ggsave(here('Results','Figures','Other', paste0('Local_cases_comparison_',Sys.Date(),'.png')),height=4.5,width=16,device='png')
 
 compare_lp <- read_csv(here('Results', 'Data', 'Iceland_posterior_2020-09-29.csv')) %>%
                 rename(y_hat=new_cases) %>%
@@ -315,10 +345,10 @@ compare_lp <- read_csv(here('Results', 'Data', 'Iceland_posterior_2020-09-29.csv
                 geom_point(data = point_d, aes(x = date, y = local, color=point_color), inherit.aes = F) +
                 scale_color_manual(values=c('orange'='#ff8c00', 'black'='black')) + 
                 scale_fill_brewer() +
-                scale_x_date(breaks = sort(c(ymd('2020-09-16'), ymd('2020-10-01'), ymd('2020-10-16'))),
+                scale_x_date(breaks = sort(c(ymd('2020-09-16'), ymd('2020-10-01'), Sys.Date())),
                              labels = icelandic_dates,
                              expand = expansion(add = 0),
-                             limits = c(ymd('2020-09-14'), ymd('2020-10-20'))) +
+                             limits = c(ymd('2020-09-14'), ymd('2020-11-03'))) +
                 scale_y_continuous(breaks = pretty_breaks(8), expand = expansion(mult = 0), limits=c(0,110)) +
                 labs(subtitle = "Innlend dagleg smit") +
                 theme(axis.title = element_blank(),
@@ -326,5 +356,47 @@ compare_lp <- read_csv(here('Results', 'Data', 'Iceland_posterior_2020-09-29.csv
                       legend.title = element_blank()) 
 compare_lp   
 ggsave(here('Results','Figures', 'Other', paste0('Local_cases_comparison_',Sys.Date(),'.png')),height=4.5,width=16,device='png')
+
+
+fill_up_dates <- ymd(c('2020-12-01', '2020-11-01'))
+
+R_estim_mod <- read_rds(here("Results", "EpiEstim", str_c("R.estimate.pred_", fit_date,".rds")))
+R_estim_m <- R_estim_mod$draws() %>% as_draws_df
+
+R_estim_p <- spread_draws(R_estim_m, R[day]) %>% 
+    group_by(day) %>% 
+    summarise(lower_50 = quantile(R, 0.25),
+              upper_50 = quantile(R, 0.75),
+              lower_60 = quantile(R, 0.2),
+              upper_60 = quantile(R, 0.8),
+              lower_70 = quantile(R, 0.15),
+              upper_70 = quantile(R, 0.85),
+              lower_80 = quantile(R, 0.1),
+              upper_80 = quantile(R, 0.9),
+              lower_90 = quantile(R, 0.05),
+              upper_90 = quantile(R, 0.95),
+              lower_95 = quantile(R, 0.025),
+              upper_95 = quantile(R, 0.975)) %>% 
+    pivot_longer(c(-day), names_to = c("which", "prob"), names_sep = "_") %>% 
+    pivot_wider(names_from = which, values_from = value) %>% 
+    mutate(prob = parse_number(prob),
+           date = Sys.Date() + day) %>% 
+    ggplot() +
+    geom_ribbon(aes(date, ymin = lower, ymax = upper,fill = factor(-prob)), alpha = 0.7) +
+    geom_hline(yintercept = 1,col='#BC3C29FF') +
+    geom_vline(xintercept = as.numeric(Sys.Date()), lty = 2) + 
+    scale_fill_brewer() +
+    scale_x_date(breaks = sort(c(fill_up_dates, head(intervention_dat$date,-1), Sys.Date())),
+                 labels = icelandic_dates,
+                 expand = expansion(add = 0),
+                 limits = c(Sys.Date()+1, ymd(end_date)+60)) +
+    scale_y_continuous(breaks = pretty_breaks(8),expand = c(0,0)) +
+    ggtitle(label = waiver(),
+            subtitle = latex2exp::TeX("Þróun smitstuðulsins ($R_t$) innanlands utan sóttkvíar")) +
+    theme(axis.title = element_blank(),
+          plot.margin = margin(5, 5, 5, 14))
+R_estim_p
+ggsave(here('Results','Figures', 'Predictions', paste0('R_estimate_',Sys.Date(),'.png')),height=4.5,width=10,device='png')
+
 
                   
