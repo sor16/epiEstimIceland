9e1219a85be54d3a7f62bcc11588554aa73ace32
karirogg <karirogg@gmail.com>
2020-12-08 15:08:13 +0000

    update for Mimir.cs.hi.is

--- Run_EpiEstim_Model.R
+++ Run_EpiEstim_Model.R
@@ -5,6 +5,14 @@ library(readxl)
 library(here)
 library(cmdstanr)
 library(posterior)
+library(readr)
+library(googlesheets4)
+
+options(gargle_oauth_email = "karirogg@gmail.com")
+gs4_auth(email = "karirogg@gmail.com")
+
+#set_cmdstan_path('~/software/cmdstan')
+set_cmdstan_path('/nfs/bin/cmdstan-2.24.0') 
 
 source('Make_EpiEstim_Q_Model.R')
 
@@ -12,5 +20,6 @@ options(mc.cores = parallel::detectCores())
 parallel:::setDefaultClusterOptions(setup_strategy = "sequential")
 
 Make_EpiEstim_Q_Model(model_name='tot.Q.late.border',
-                        start_date='2020-02-28',
-                        end_date=Sys.Date())
+                      start_date='2020-02-28',
+                      end_date=Sys.Date(),
+                      pred_d = NULL)
